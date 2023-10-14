529dc5076141e9a149d0f6cdf823c8f285c0ec87
nsdigirolamo <nsdigirolamo@gmail.com>
2023-10-14 14:39:58 -0400

    Seriously refactored code. No functional changes but lots of rearranging.

--- public/scripts/tournament-viewer.js
+++ public/scripts/tournament-viewer.js
@@ -1,18 +1,78 @@
 import { Tournament, getTournament, postTournament, deleteTournament, decodeTeamData, decodeResultsData, postParticipant } from "./firebase-utils.js";
 
 /**
- * Adds the current user to the list of potential participants for the Tournament with the given ID.
- * @param {Tournament} tournament 
+ * Creates the div that informs users when the Tournament starts.
+ * @param {Date} start_date
+ * @returns {HTMLElement}
  */
-function addCurrentUserToTournament (id) {
-    postParticipant(id, firebase.auth().currentUser);
+function createTimeDiv (start_date) {
+    const time_div = document.createElement("div");
+    time_div.id = "time-info";
+    time_div.textContent = `This tournament starts on ${start_date.toDateString()}. You can join this tournament at any time before that date.`;
+    return time_div;
+}
+
+/**
+ * Creates the button that allows users to join a Tournament.
+ * @return {HTMLElement}
+ */
+function createJoinButton () {
+    const join_button = document.createElement("button");
+    join_button.id = "join-button";
+    join_button.textContent = "Join Tournament";
+    return join_button;
+}
+
+/**
+ * Creates the button that allows a creator to delete their Tournament.
+ * @return {HTMLElement}
+ */
+function createRemoveButton () {
+    const remove_button = document.createElement("button");
+    remove_button.id = "remove-button";
+    remove_button.textContent = "Delete Tournament";
+    return remove_button;
+}
+
+/**
+ * Creates various elements depending on the state of the given Tournament. These elements will allow
+ * a user to join the tournament if it's start date is some time in the future.
+ * @param {Tournament}
+ * @return {HTMLElement}
+ */
+function createJoinInfoElements (tournament) {
+
+    const current_date = new Date();
+    const start_date = tournament.start_date == null ? null : new Date(tournament.start_date);
+
+    const div = document.createElement("div");
+    div.id = "join-info";
+
+    if (tournament.start_date && current_date < start_date) {
+    
+        const time_div = createTimeDiv(start_date)
+        div.appendChild(time_div);
+
+        const join_button = createJoinButton();
+        div.appendChild(join_button);
+
+        join_button.addEventListener("click", () => {
+            postParticipant(tournament.id, firebase.auth().currentUser);
+        });
+
+    } else {
+        div.textContent = "This tournament is closed to new members.";
+    }
+
+    return div;
 }
 
 /**
  * Loads a div containing information about the given tournament.
- * @param {Tournament} tournament 
+ * @param {Tournament} tournament
+ * @return {HTMLElement} 
  */
-function loadInfoDiv (tournament) {
+function createInfoDiv (tournament) {
 
     const info_div = document.createElement("div");
     info_div.id = "tournament-info";
@@ -23,46 +83,7 @@ function loadInfoDiv (tournament) {
         <h3>Created by ${tournament.creator_display_name}</h3>
     `;
 
-    document.querySelector("#page-view").appendChild(info_div);
-
-    if (firebase.auth().currentUser.uid == tournament.creator_uid) {
-
-        const remove_button = document.createElement("button");
-        remove_button.id = "remove-button";
-        remove_button.textContent = "Delete Tournament";
-        info_div.appendChild(remove_button);
-
-        document.querySelector("#remove-button").addEventListener("click", () => {
-            deleteTournament(tournament.id);
-        });
-
-    } else {
-
-        const current_date = new Date();
-        const start_date = tournament.start_date == null ? null : new Date(tournament.start_date);
-
-        if (tournament.start_date && current_date < start_date) {
-
-            const time_div = document.createElement("div");
-            time_div.id = "time";
-            time_div.textContent = `This tournament starts on ${start_date.toDateString()}. You can join this tournament at any time before that date.`;
-            info_div.appendChild(time_div);
-
-            const join_button = document.createElement("button");
-            join_button.id = "join-button";
-            join_button.textContent = "Join Tournament";
-            info_div.appendChild(join_button);
-
-        } else {
-
-            const time_div = document.createElement("div");
-            time_div.id = "time";
-            time_div.textContent = "This tournament is closed to new members.";
-            info_div.appendChild(time_div);
-
-        }
-
-    }
+    return info_div;
 }
 
 /**
@@ -88,10 +109,17 @@ export function loadViewer (tournament_id) {
         tournament.bracket_data.teams = decodeTeamData(tournament.bracket_data.teams);
         tournament.bracket_data.results = decodeResultsData(tournament.bracket_data.results);
 
-        loadInfoDiv(tournament);
-        loadBracketDiv(tournament);
+        const info_div = createInfoDiv(tournament);
+        document.querySelector("#page-view").appendChild(info_div);
 
         if (firebase.auth().currentUser.uid == tournament.creator_uid) {
+
+            const remove_button = createRemoveButton();
+            info_div.appendChild(remove_button);
+
+            remove_button.addEventListener("click", () => { 
+                deleteTournament(tournament.id);
+            });
 
             $(function() {
                 $('#tournament-bracket').bracket({
@@ -105,9 +133,8 @@ export function loadViewer (tournament_id) {
 
         } else {
 
-            document.querySelector("#join-button").addEventListener("click", () => {
-                addCurrentUserToTournament(tournament);
-            });
+            const join_info_elements = createJoinInfoElements(tournament);
+            document.querySelector("#page-view").appendChild(join_info_elements);
 
             /**
              * The save callback is absent if the current user is not the tournament's creator.
@@ -121,6 +148,8 @@ export function loadViewer (tournament_id) {
             });
 
         }
+
+        loadBracketDiv();
 
     }).catch(error => {
         console.log(error);
